a6ac6f20ccf8974e1bcafc00d123c4b32aff1e16
FaultyPine <gclark1013@gmail.com>
2026-08-17 10:49:25 -0700

    moving build artifacts into build folder

--- build.bat
+++ build.bat
@@ -34,13 +34,13 @@ if errorlevel 1 goto :fail
 set ZydisArgs= -I%EXTERNAL_ROOT%/zydis/dependencies/zycore/include -I%EXTERNAL_ROOT%/zydis/include/Zydis -I%EXTERNAL_ROOT%/zydis/include -L%EXTERNAL_ROOT%/zydis/build/RelWithDebInfo -lZydis -L%EXTERNAL_ROOT%/zydis/build/zycore/RelWithDebInfo -lZycore -DZYDIS_STATIC_BUILD -DZYCORE_STATIC_BUILD
 set DynasmArgs= -I%ROOT% -I%EXTERNAL_ROOT%/LuaJIT/dynasm
 
-"%CLANG%" main.c -o main.exe -g
+"%CLANG%" main.c -o %BUILD%/main.exe -g
 if errorlevel 1 goto :fail
 
-"%CLANG%" "%DYNASM_INJECTION_C%" -o injection.dll -g -shared %ZydisArgs% %DynasmArgs%
+"%CLANG%" "%DYNASM_INJECTION_C%" -o %BUILD%/injection.dll -g -shared %ZydisArgs% %DynasmArgs%
 if errorlevel 1 goto :fail
 
-"%CLANG%" tester.c -o tester.exe -g -luser32
+"%CLANG%" tester.c -o %BUILD%/tester.exe -g -luser32
 if errorlevel 1 goto :fail
 
 echo [build] Success.
--- run.bat
+++ run.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal
 
-set "testerExe=tester.exe"
+set "ROOT=%~dp0"
+if "%ROOT:~-1%"=="\" set "ROOT=%ROOT:~0,-1%"
+set "BUILD=%ROOT%\build"
+
+set "testerExe=%BUILD%/tester.exe"
 for /f "tokens=*" %%A in ('powershell -Command "(Start-Process '%testerExe%' -PassThru).Id"') do set "PID=%%A"
 
 echo Launched test process at PID = %PID%
-main.exe --pid %PID%
+"%BUILD%/main.exe" --pid %PID%
 
 endlocal
 exit /b 0
