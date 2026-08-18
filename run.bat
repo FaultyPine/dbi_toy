1a34ea38086f2ef494345f810058b9ad57c10042
FaultyPine <gclark1013@gmail.com>
2026-08-18 13:54:52 -0700

    allow run.bat to have args passed to it, for stuff like --exeName

--- run.bat
+++ run.bat
@@ -5,12 +5,24 @@ set "ROOT=%~dp0"
 if "%ROOT:~-1%"=="\" set "ROOT=%ROOT:~0,-1%"
 set "BUILD=%ROOT%\build"
 
+if "%~1"=="" (
+    goto :testerRun
+)
+else (
+    "%BUILD%/main.exe" %*
+    if errorlevel 1 goto :fail
+    goto :success
+)
+
+:testerRun
 set "testerExe=%BUILD%/tester.exe"
 for /f "tokens=*" %%A in ('powershell -Command "(Start-Process '%testerExe%' -PassThru).Id"') do set "PID=%%A"
 
 echo Launched test process at PID = %PID%
 "%BUILD%/main.exe" --pid %PID%
+if errorlevel 1 goto :fail
 
+:success
 endlocal
 exit /b 0
 
